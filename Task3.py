e5af027081df92d58b6466ed37c7197e569a1079
Allano13 <refmanallano@gmail.com>
2025-04-22 02:09:44 +0700

    Initial commit: Task1-Task4

---
+++ Task3.py
@@ -0,0 +1,27 @@
+# Задание 3: Проверка четности
+# Напиши скрипт, который запрашивает у пользователя число и выводит, является ли оно четным или нечетным, в формате:
+# Число <число> четное
+import sys  # Для проверки на крайние значения float
+
+def enter_number():
+    """Запрос числа пользователя с проверкой на ошибки."""
+    while True:
+        try:
+            number = float(input("Введите число: "))
+            if abs(number) > sys.float_info.max:
+                print("Ошибка: Число слишком большое.")
+                continue
+            if not number.is_integer():
+                print("Ошибка: Введите целое число (например, 5).")
+                continue
+
+
+            return int(number)
+        except ValueError:
+            print("Ошибка: Введите корректное число (например, 5).")
+
+
+user_number = enter_number()
+definition = "четное" if user_number % 2 == 0 else "нечетное"  # Проверка четности числа
+
+print(f"Число {user_number} {definition}")
