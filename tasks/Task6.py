2f408d8c1c6ef138daf986cf73a012d12f974e0c
Allano13 <refmanallano@gmail.com>
2025-04-22 20:03:23 +0700

    Task6.py V_1.0

--- tasks/Task6.py
+++ tasks/Task6.py
@@ -7,22 +7,29 @@
 
 
 def main():
-    """Цикл основной функции"""
+    """Основной цикл программы.
+
+    Запрашивает у пользователя выбор действия и выполняет конвертацию температуры.
+    Обрабатывает прерывание через Ctrl+C.
+    """
 
     try:
         while True:
             continue_choice = input(
                 "Конвертировать температуру? (y/n, Ctrl+C): "
             )  # Проверка хочет ли пользователь продолжить в цикле
+            if not continue_choice:
+                print("Ошибка: введите 'y' или 'n'.")
+                continue
             if continue_choice == "y":  # основной цикл
                 scale = scale_input()
                 value = value_input()
                 if scale == "f":
                     result = celsius_calculations(value)
-                    print(f"{round(value, 2)}°F = {round(result, 2)}°C")  # Вывод 1
+                    print(f"{value:.2f}°F = {result:.2f}°C".rstrip("0").rstrip("."))
                 else:
                     result = fahrenheit_calculations(value)
-                    print(f"{round(value, 2)}°C = {round(result, 2)}°F")  # Вывод 2
+                    print(f"{value:.2f}°C = {result:.2f}°F".rstrip("0").rstrip("."))
             elif continue_choice == "n":  # Выход из программы
                 print("Завершение программы")
                 break
@@ -36,41 +43,67 @@ def main():
 
 
 def fahrenheit_calculations(number):
-    """Перевод Цельсий в Фаренгейт"""
+    """Перевод Цельсий в Фаренгейт
+
+    Args:
+        number(float): Температура в Цельсиях
+    Returns:
+        float: температура в Фаренгейтах
+    """
 
     return number * 1.8 + 32
 
 
 def celsius_calculations(number):
-    """Перевод Фаренгейт в Цельсий"""
+    """Переводит температуру из Фаренгейтов в Цельсии.
+
+    Args:
+        number (float): Температура в Фаренгейтах.
+    Returns:
+        float: Температура в Цельсиях.
+    """
 
     return (number - 32) / 1.8
 
 
 def value_input():
-    """Ввод пользовательского значения температуры"""
+    """Запрашивает у пользователя значение температуры.
+
+    Returns:
+        float: Введённая температура.
+    Raises:
+        ValueError: Если введено некорректное значение.
+    """
 
     # цикл с проверкой на вводимое число
     while True:
+        number = input("Введите температуру: ")
+        if not number:
+            print("Ошибка: введите число")
+            continue
         try:
-            number = float(input("Введите температуру: "))
-            return int(number)
-
+            return float(number)
         except ValueError:
             print("Ошибка: введите число")
 
 
 def scale_input():
-    """Ввод пользовательского значения шкалы"""
+    """Запрашивает у пользователя шкалу температуры (F или C).
+
+    Returns:
+        str: Выбранная шкала ("f" или "c").
+    """
 
     # Цикл с проверкой на вводимое значение
     while True:
         scale_user = input("Введите шкалу (F/C): ").lower()
+        if not scale_user:
+            print("Ошибка: введите правильное значение(F/C)")
+            continue
         if scale_user == "f" or scale_user == "c":
             return scale_user
         else:
             print("Ошибка: введите правильное значение(F/C)")
-
 
 # основное тело программы
 if __name__ == "__main__":
